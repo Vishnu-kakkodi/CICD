ae44d2cef2fadb3a3ad74670a50c047d851eefcd
melvincherian <melvincherian0190@gmail.com>
2026-06-01 12:47:28 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend Running",
+    environment: process.env.NODE_ENV,
+    port: process.env.PORT
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
